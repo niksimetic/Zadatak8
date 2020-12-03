6c03321f64e687b2b8c7d249cfa6549a6163ca75
niksimetic <55134764+niksimetic@users.noreply.github.com>
2020-12-03 10:55:24 +0100

    Add files via upload

---
+++ nikSimeticZadatak8/nikSimeticZadatak8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nikSimeticZadatak8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sport Cruling = new Sport("Curling");
+            Sport Vaterpolo = new Sport("Vaterpolo", true);
+            Sport Nogomet = new Sport("Nogomet", true, 11);
+        }
+    }
+}
---
+++ nikSimeticZadatak8/nikSimeticZadatak8/Sport.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nikSimeticZadatak8
+{
+    class Sport
+    {
+        private string naziv;
+        private bool igraLiSeLoptom;
+        private byte brojIgraca;
+
+        public string Naziv { get => naziv; set => naziv = value; }
+        public bool IgraLiSeLoptom { get => igraLiSeLoptom; set => igraLiSeLoptom = value; }
+        public byte BrojIgraca { get => brojIgraca; set => brojIgraca = value; }
+
+        public Sport(string naziv)
+        {
+            this.naziv = naziv;
+        }
+        public Sport(string naziv, bool igraLiSeLoptom)
+        {
+            this.naziv = naziv;
+            this.igraLiSeLoptom = igraLiSeLoptom;
+        }
+        public Sport(string naziv, bool igraLiSeLoptom, byte brojIgraca)
+        {
+            this.naziv = naziv;
+            this.igraLiSeLoptom = igraLiSeLoptom;
+            this.brojIgraca = brojIgraca;
+        }
+    }
+}
